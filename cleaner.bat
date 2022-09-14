291308650122db580abdffbbdc9c8da9c34d64b4
AdamPRankin <mrasylumescapee@gmail.com>
2022-09-14 10:33:42 -0600

    initial upload

---
+++ cleaner.bat
@@ -0,0 +1,12 @@
+echo cmd delete all files in folder
+
+del "C:\ProgramData\Microsoft\Windows\WER\ReportQueue\*.*" /s /f /q
+del "C:\Windows\Temp\*.*" /s /f /q
+del "C:\$Recycle. Bin\adam\*.*" /s /f /q
+del "C:\Windows\Downloaded Program Files\*.*" /s /f /q
+del "C:\Windows\LiveKernelReports\*.*" /s /f /q
+
+
+
+
+echo Done!
